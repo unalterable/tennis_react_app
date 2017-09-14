72b938774f9dfaff76afa7d05969b58c828df4f5
Oliver Diamond <diamond.oliver@gmail.com>
2017-09-14 13:30:04 +0100

    better tests passing

--- src/tennis_rules.js
+++ src/tennis_rules.js
@@ -1,8 +1,24 @@
 "use-strict";
 
+const TENNIS_SCORES = ["0", "15", "30", "40"]
+
 const TENNIS_RULES = {
-  score: function(){
-    return {scorer: "15", opponent:"0"};
+  score: function(currScore){
+    switch(currScore.scorer){
+      default:
+        return {scorer: TENNIS_SCORES[TENNIS_SCORES.indexOf(currScore.scorer) + 1],
+                opponent: currScore.opponent};
+      case "40":
+        switch(currScore.opponent){
+          case "A":
+            return {scorer: "40", opponent: "40"};
+          case "40":
+            return {scorer: "A", opponent: "40"};
+          default:
+        }
+        case "A":
+          return {scorer: "0", opponent: "0", winner: true};
+    }
   }
 };
 
