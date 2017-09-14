45222e35a415ae62ce43ee341664942940f3ff43
Oliver Diamond <diamond.oliver@gmail.com>
2017-09-14 13:12:05 +0100

    better tests failing

--- test/tennis_rules_spec.js
+++ test/tennis_rules_spec.js
@@ -5,7 +5,27 @@ const expect = require('chai').expect;
 const sut = require('../src/tennis_rules')
 
 describe("Tennis Scoring", function(){
-  it("Scoring works ", function(){
+  it("Basic scoring works correctly", function(){
     expect(sut.score({scorer: "0", opponent: "0"})).to.eql({scorer: "15", opponent:"0"});
-  })
-})
+    expect(sut.score({scorer: "15", opponent: "0"})).to.eql({scorer: "30", opponent:"0"});
+    expect(sut.score({scorer: "15", opponent: "15"})).to.eql({scorer: "30", opponent:"15"});
+    expect(sut.score({scorer: "30", opponent: "0"})).to.eql({scorer: "40", opponent:"0"});
+    expect(sut.score({scorer: "30", opponent: "40"})).to.eql({scorer: "40", opponent:"40"});
+  });
+  it("Winning game", function(){
+    expect(sut.score({scorer: "40", opponent: "0"})).to.eql({scorer: "0", opponent:"0", winner: true});
+    expect(sut.score({scorer: "40", opponent: "15"})).to.eql({scorer: "0", opponent:"0", winner: true});
+    expect(sut.score({scorer: "40", opponent: "30"})).to.eql({scorer: "0", opponent:"0", winner: true});
+  });
+  it("Advantage", function(){
+    expect(sut.score({scorer: "40", opponent: "40"})).to.eql({scorer: "A", opponent:"40"});
+    expect(sut.score({scorer: "40", opponent: "40"}).winner).to.not.exist;
+  });
+  it("Back to deuce", function(){
+    expect(sut.score({scorer: "40", opponent: "A"})).to.eql({scorer: "40", opponent:"40"});
+    expect(sut.score({scorer: "40", opponent: "A"}).winner).to.not.exist;
+  });
+  it("Winning from advantage", function(){
+    expect(sut.score({scorer: "A", opponent: "40"})).to.eql({scorer: "0", opponent:"0", winner: true});
+  });
+});
